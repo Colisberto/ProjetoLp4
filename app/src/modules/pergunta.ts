1dbfa685543e3e6ec89e027af357c78a4da83f03
Colisberto <beto_podium@hotmail.com>
2018-10-31 12:31:23 -0300

    Criação modulo( cadastro-pergunta), criado classe (Pergunta) em modulo, criação do botão pergunta em app/home

---
+++ app/src/modules/pergunta.ts
@@ -0,0 +1,14 @@
+/**   COLISBERTO
+ *  Para ser usado para criação do objeto pergunta e seus atributos */
+
+export class Pergunta {
+    id: number;
+    pergunta: string;
+    opcao_a: string;
+    opcao_b: string;
+    opcao_c: string;
+    opcao_d: string;
+    opcao_e: string;
+    categoria : number;
+   
+}
--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ListaUsuarioPage } from '../lista-usuario/lista-usuario';
+import { CadastroPerguntaPage } from '../cadastro-pergunta/cadastro-pergunta';
 
 @Component({
   selector: 'page-home',
@@ -9,12 +10,22 @@ import { ListaUsuarioPage } from '../lista-usuario/lista-usuario';
 export class HomePage {
 
   listaUsuarios = ListaUsuarioPage;
+  /** Acrescentado para ser usado na chamada da tela */
+  cadastraPerguntas = CadastroPerguntaPage;
   constructor(public navCtrl: NavController) {
 
   }
 
   avancarListaUsuario(){
     this.navCtrl.push(ListaUsuarioPage.name);
+  }
+  
+  /**   OBS: COLISBERTO
+   * Acredito que o erro pode estar aqui para chamar a tela de cadastro em 
+   * cadastro-pergunta/cadastro-pergunta.html
+   */
+  avancarCadastraPergunta(){
+    this.navCtrl.push(CadastroPerguntaPage.name);
   }
 
 }
